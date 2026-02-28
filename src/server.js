ac807da60fee04efe02486c2ee2e6d7e829acba6
Wahana Express <suryafarsya16@gmail.com>
2026-02-28 15:48:23 +0700

    prepare for vercel deploy

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 import express from "express"
+import cors from "cors"
 import serverless from "serverless-http"
 import routes from "../src/routes"
 
 const app = express()
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173", // frontend local kamu
+    // "https://namafrontend.vercel.app" 
+  ],
+  credentials: true
+}))
 
 app.use(express.json())
 app.use("/api", routes)
